8474f166e8a06a55a3b7c9521dc8b672726051c0
Aste21 <131811810+Aste21@users.noreply.github.com>
2023-11-03 20:14:30 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,46 +1,13 @@
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.StandardOpenOption;
-import java.util.List;
 
-public class Main {
-
-    public static void main(String[] args) {
-        String inputFileName = "InputData.txt";
-        String outputFileName = "MyData.txt";
-        int rewrittenDates = convertDate(inputFileName, outputFileName);
-        System.out.println("Number of rewritten dates: " + rewrittenDates);
-    }
-
-    static int convertDate(String inputFileName, String outputFileName) {
-        int rewrittenDates = 0;
-        Path pathInput = Path.of(inputFileName);
-        Path pathOutput = Path.of(outputFileName);
-
-        try {
-            Files.writeString(pathOutput, "");
-            List<String> allLines = Files.readAllLines(pathInput);
-            String[] regex = { "\\d{2}/\\d{2}/\\d{4} \\w+", "\\d{2}/\\d{1}/\\d{4} \\w+", "\\d{4}-\\d{2}-\\d{2} \\w+", "\\w+ \\d{2}.\\d{2}.\\d{4}" };
-            String[] pattern = { "(\\d{2})/(\\d{2})/(\\d{4}) (\\w+)", "(\\d{2})/(\\d{1})/(\\d{4}) (\\w+)", "(\\d{4})-(\\d{2})-(\\d{2}) (\\w+)", "(\\w+) (\\d{2}).(\\d{2}).(\\d{4})" };
-            MyData lastWrittenDate = null;
-            for (String line : allLines) {
-                for (int i = 0; i < regex.length; i++) {
-                    if (line.matches(regex[i]))
-                    {
-                        MyData mydata = MyData.parseDate(line, pattern[i], i);
-                        if (mydata != null && !mydata.isEqual(lastWrittenDate)) {
-                            Files.writeString(pathOutput, mydata.formatAsString() + System.lineSeparator(), StandardOpenOption.APPEND);
-                            lastWrittenDate = mydata;
-                            rewrittenDates += 1;
-                        }
-                        break;
-                    }
-                }
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+public class Main { //z main jest poroblem 
+    public static void main(String[] args){
+        try{
+            int rewrittenDates = FileDataConverter.fileDataConvert("InputData.txt", "MyData.txt");
+            System.out.println("Number of rewritten dates: " + rewrittenDates);
+        }catch(IOException e){
+            System.out.println("IOException ensasd");
         }
-        return rewrittenDates;
     }
 }
+ 
